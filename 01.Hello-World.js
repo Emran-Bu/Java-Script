d3bda835598105b4e3400a6eb157a3f5b45c9d95
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-17 18:32:16 +0600

    Hello-World text print out in first java script

---
+++ 01.Hello-World.js
@@ -0,0 +1,17 @@
+console.log('Hello World')
+console.log('I love bangladesh')
+console.log('999')
+console.log('9+9')
+console.log(9+9)
+console.log(8-9)
+console.log(9*9)
+console.log(9/9)
+console.log('number' +9)
+console.log('number'+'number')
+console.log('number' + 'number')
+console.log('number ' + ' number')
+console.log(60.707)
+console.log('my+my')
+console.log('My favourite no: ' +99);
+console.log('My favourite no: ' +9+9);
+// console.log('My favourite no: ' 9+9);
