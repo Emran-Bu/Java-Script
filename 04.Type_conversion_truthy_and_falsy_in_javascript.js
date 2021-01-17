a896e03a0f0033221a4a406d8a0bd6b06e7a9617
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-17 18:35:53 +0600

    Type_conversion_truthy_and_falsy

---
+++ 04.Type_conversion_truthy_and_falsy_in_javascript.js
@@ -0,0 +1,29 @@
+var str = '10'
+var n = 10
+
+var ccc = true
+
+console.log(str * n)
+
+console.log(str + n)
+console.log(Number(str))
+console.log(Number.parseInt(str))
+console.log(n.toString());
+
+// falsy value
+''
+0
+null
+undefined
+NaN
+
+console.log(Boolean(Infinity));
+console.log(Boolean(''));
+console.log(Boolean("ubhjhjhjh"));
+console.log(Boolean('Infinity'));
+console.log(Boolean(-Infinity));
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(555));
