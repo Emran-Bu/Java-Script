73ea01d7c62b9c4bc3817ee71d89a6307b7db989
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-17 18:33:56 +0600

    variable declaration

---
+++ 02.variable.js
@@ -0,0 +1,22 @@
+var name="Emran"
+var age= 22
+
+var color = "blue"
+var number = 789
+
+console.log(name)
+console.log(age)
+
+console.log(color)
+console.log(number)
+
+console.log(name +" Knows Javascript")
+console.log("His age is only: " + age);
+
+console.log('my favourite color is ' + color)
+console.log('my favourite number is: ' + number)
+
+var amename9NNN545 ="Emran"
+console.log(amename9NNN545)
+
+console.log('ffff' +amename9NNN545)
