32059b4aed30e88afde2dc038a93e030a0428757
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-18 08:16:31 +0600

    basic_number_in_javascript

---
+++ 07.basic_number_in_javascript.js
@@ -0,0 +1,11 @@
+var n = 1200
+var f = 3.1416
+var nn = Number('45' )
+var ff = Number(36.36)
+
+console.log(nn + 45);
+console.log(Number.parseInt(ff))
+console.log(Number.parseFloat(ff))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
