e4b5d90fdd59ab44d8fb913305b9dd0a5ecca17a
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-18 08:15:48 +0600

    basic_string_in_javascript

---
+++ 06.basic_string_in_javascript.js
@@ -0,0 +1,11 @@
+var str1 = 'emran'
+var str2 = "Eshan"
+var str3 = `ehav`
+
+var str4 = String('this is string')
+var str5 = String(45)
+var str6 = String(20.20365)
+
+console.log(str1, str2, str3)
+
+console.log(str4, str5, str6)
