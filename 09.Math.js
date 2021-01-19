522fdfdd826610ca8b0bf738c9dd78b74d4e028b
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-19 19:21:43 +0600

    Math in java script

---
+++ 09.Math.js
@@ -0,0 +1,22 @@
+console.log(Math.E)
+console.log(Math.PI)
+
+var n = 9.545
+console.log(Math.abs(n))
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+
+console.log(Math.max(100, 200, 300, 400))
+console.log(Math.min(100, 200, 300, 400))
+
+console.log(Math.pow(2, 3))
+console.log(Math.pow(2, 4))
+
+console.log(Math.sqrt(4))
+console.log(Math.sqrt(64))
+
+console.log(Math.round(Math.random() * 50+1))
+// console.log(Math.pow(2, 4))
+console.log(Math.sin(90))
+console.log(Math.cos(90))
