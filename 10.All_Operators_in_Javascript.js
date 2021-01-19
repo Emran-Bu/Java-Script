5cb5aec8437bd8dd46f7c08e9382de01fc8a7349
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-19 19:22:34 +0600

    All_Operators_in_Javascript

---
+++ 10.All_Operators_in_Javascript.js
@@ -0,0 +1,74 @@
+// Arithmetic operator
+// + - * / % ++ --
+// % Modulas
+// Increment
+// 1. post increment(a++)
+// 2. pre increment(++a)
+// decrement
+// 1. post decrement(a--)
+// 2. pre decrement(--a)
+
+var a = 12
+var b = 5
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+console.log(++a)
+console.log(a++)
+console.log(a)
+console.log(++a)
+console.log(++a)
+
+var c = 12
+console.log(c--)
+console.log(--c)
+
+// Asignment operator
+var a = 10
+var b = 20
+a+=b
+console.log(a)
+
+a-=b
+console.log(a)
+
+a*=b
+console.log(a)
+
+a/=b
+console.log(a)
+
+a%=b
+console.log(a)
+
+// comperision operator
+var a = 10
+var b = 20
+console.log(a==b) //false
+console.log(a!=b) //true
+console.log(a>b) //false
+console.log(a<b) //true
+console.log(a>=b) //false
+console.log(a<=b) //true
+
+var c = '10'
+var d = 10
+
+console.log(c==d) //true
+console.log(c===d) //false
+console.log(c !== d) //false
+
+
+// Logical operator
+// && || !
+
+// Bitwise operator
+// & | ~ ^ << >>
+//
+// type of operator
+
+console.log(typeof 10)
+console.log(typeof 'string')
