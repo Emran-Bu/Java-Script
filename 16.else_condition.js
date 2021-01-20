96345013f17413c5722d3d4261d8d88db9e9c127
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-20 19:19:52 +0600

    else_condition

---
+++ 16.else_condition.js
@@ -0,0 +1,21 @@
+var a = 5
+var b = 6
+
+if (a>b){
+  console.log('A is greater than B')
+}
+
+else{
+  console.log('B is greater than A')
+}
+
+
+var c = 7
+
+if (c%2 === 0){
+  console.log(c + ' is even number')
+}
+
+else{
+ console.log(c + ' is odd number')
+}
