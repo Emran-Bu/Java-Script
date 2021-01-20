983ee02c11c8a6145841b3fe9729a4f2e80bb5ee
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-20 19:16:06 +0600

    And_OR_operation_shorthand

---
+++ 14.And_OR_operation_shorthand.js
@@ -0,0 +1,30 @@
+var name = ''
+
+var fullname = name || 'Emran Hasan'
+console.log(fullname)
+
+
+var isok = true
+
+isok && console.log('Everything is ok')
+
+
+
+// var name = 'xyz'
+//
+// var fullname = name || 'Emran Hasan'
+// console.log(fullname)
+
+// var h = false
+//
+// if (h) {
+//   console.log('yes')
+// }
+
+// if (name.length===0){
+//   name = 'emran'
+// }
+// else {
+//   fullname = name
+// }
+// console.log(name)
