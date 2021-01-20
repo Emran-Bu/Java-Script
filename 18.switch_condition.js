ea99dcab8fba87988cfcd33350f968c91768fcc2
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-20 19:21:30 +0600

    switch_condition

---
+++ 18.switch_condition.js
@@ -0,0 +1,84 @@
+switch (2){
+  case 0:
+  console.log('day')
+  break
+
+  case 2:
+  console.log('month')
+}
+
+var date = new Date()
+
+// 0-sunday, 1-monday, 2-tuesday
+var today = date.getDay()
+
+switch (today){
+    case 0:
+    console.log('Today is sunday')
+    break
+
+    case 1:
+    console.log('today is monday')
+    break
+
+    case 2:
+    console.log('today is tuesday')
+    break
+
+    case 3:
+    console.log('today is wednesday')
+    break
+
+    case 4:
+    console.log('today is tursday')
+    break
+
+    case 5:
+    console.log('today is friday')
+    break
+
+    case 6:
+    console.log('today is suturday')
+    break
+
+    default:
+    console.log('Not a Valid Number')
+}
+
+var today = date.getDay()
+
+//valid unvalid number
+// switch (gh)
+
+switch (7){
+    case 0:
+    console.log('Today is sunday')
+    break
+
+    case 1:
+    console.log('today is monday')
+    break
+
+    case 2:
+    console.log('today is tuesday')
+    break
+
+    case 3:
+    console.log('today is wednesday')
+    break
+
+    case 4:
+    console.log('today is tursday')
+    break
+
+    case 5:
+    console.log('today is friday')
+    break
+
+    case 6:
+    console.log('today is suturday')
+    break
+
+    default:
+    console.log('Not a Valid Number')
+}
