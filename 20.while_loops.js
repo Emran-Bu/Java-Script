f12266ace8f5755e9eff80d92989b627e969f68b
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-21 07:28:45 +0600

    while_loops

---
+++ 20.while_loops.js
@@ -0,0 +1,27 @@
+var isRunning = true
+while (isRunning){
+    var rand = Math.floor(Math.random() * 10 + 1)
+    if (rand === 9){
+      console.log('Winner Winner Chickem Dinner')
+      isRunning = false
+    }
+
+    else{
+      console.log('You have got ' + rand)
+    }
+}
+
+
+
+
+// var i = 0
+// while (i<10) {
+//   console.log('Emran')
+//   i++
+// }
+
+// var i = 0
+// while (i<10) {
+//   console.log((i+1) + ' Emran')
+//   i++
+// }
