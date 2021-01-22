58f5e566b2d2fe6c0fcb4e0f99aa2a987c828c9c
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-22 07:55:19 +0600

    string_method

---
+++ 26.string_method.js
@@ -0,0 +1,18 @@
+var a = 'i am'
+var b = 'EMRAN hasan'
+var c = a.concat(' ' ,b)
+console.log(c)
+var d = c.substr(5)
+console.log(d)
+
+console.log(c.charAt(5))
+console.log(c.startsWith('i'))
+console.log(c.startsWith('I am'))
+console.log(c.endsWith('an'))
+console.log(b.toLowerCase())
+console.log(a.toUpperCase())
+console.log('      emran      ')
+console.log('      emran      '.trim())
+console.log(c.split())
+console.log(c.split(''))
+console.log(c.split(' '))
