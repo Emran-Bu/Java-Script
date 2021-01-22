9891d3e61a827af3f4a416e7ee8e404ae1bf382d
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-22 07:53:35 +0600

    continue_statement

---
+++ 24.continue_statement.js
@@ -0,0 +1,23 @@
+// for (i=1; i<10; i++) {
+//   if (i===3) {
+//     continue
+//   } else {
+//     console.log(i)
+//   }
+// }
+
+for (i=1; i<10; i++) {
+  if (i===3 || i == 7) {
+    continue
+  } else {
+    console.log(i)
+  }
+}
+
+// for (i=1; i<10; i++) {
+//   if (i===3 || i == 7 || i == 5) {
+//     continue
+//   } else {
+//     console.log(i)
+//   }
+// }
