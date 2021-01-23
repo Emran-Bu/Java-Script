757871e97147e0f4f405730e5265c84b446e8a47
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-23 18:18:54 +0600

    string_length

---
+++ 27.string_length.js
@@ -0,0 +1,16 @@
+var str = 'emran'
+
+var length = 0
+
+while (true) {
+  if (str.charAt(length) === '' ) {
+    break
+  } else {
+    length++
+  }
+}
+console.log(length)
+
+// different rules
+console.log('hfgdtgfuyetdfrtgytre'.length)
+console.log(str.length)
