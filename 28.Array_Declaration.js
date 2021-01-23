fd31e8751b6908ea455f16aeab7bc30532696f84
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-23 18:19:49 +0600

    Array_Declaration

---
+++ 28.Array_Declaration.js
@@ -0,0 +1,21 @@
+// array declare 1st rule
+var arr = [1, 2, 3, 4, 5, 6]
+// array add
+arr[6] = 7
+arr[10] = 11
+console.log(arr)
+
+// array length count
+
+console.log(arr.length)
+
+// array length
+
+console.log(arr[0])
+console.log(arr[3])
+console.log(arr[4])
+
+// array declare 1st rule
+var arr2 = Array(1, 2, 3)
+arr2[3] = 4
+console.log(arr2)
