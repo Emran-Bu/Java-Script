09a1913700e585ffeba6274590b27298f6ec4d53
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-23 18:22:35 +0600

    array_insert_remove_and_replace

---
+++ 30.array_insert_remove_and_replace.js
@@ -0,0 +1,34 @@
+var arr = [1, 2, 3, 4, 5]
+//insert array in alternet
+// arr[3] = 8
+//insert array in last
+// arr.push(6)
+//insert array in last
+// arr.unshift(9)
+
+//one data insert 6
+// arr.splice(2, 0, 6)
+//two data insert 6, 10
+// arr.splice(2, 0, 6, 10)
+// console.log(arr)
+
+//remove array in fixed length
+// arr[3]=null
+// arr[0]=undefined
+// console.log(arr.length)
+
+//remove array in last
+// arr.pop()
+
+//remove array in first
+// arr.shift()
+//one data reomove
+// arr.splice(2, 1)
+//two data reomove
+// arr.splice(2, 2)
+
+//data replace/update/insert array
+arr.splice(2, 1, 50)
+//insert array in alternet
+// arr[3] = 8
+console.log(arr)
