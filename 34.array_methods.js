e78b78ec91d4732ce52c2b127c91641215328917
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-25 22:27:21 +0600

    array_methods

---
+++ 34.array_methods.js
@@ -0,0 +1,26 @@
+var arr = [4, 5, 6, 9]
+var arr2 = [3, 7, 8, 4]
+
+//array method
+// console.log(arr.join(''))
+// console.log(arr.join(' '))
+// console.log(arr.join('|'))
+// console.log(arr.join(','))
+
+// arr.fill(true)
+// arr.fill(0)
+// console.log(arr)
+
+// var arr3 = arr.concat(arr2)
+// console.log(arr3)
+
+console.log(Array.isArray(arr))
+
+var arr4 = Array.from(arr)
+console.log(arr4)
+
+// arr4[0] = 20
+// console.log(arr)
+// var arr4 = arr2
+// console.log(arr4)
+//console.log(arr.sort())
