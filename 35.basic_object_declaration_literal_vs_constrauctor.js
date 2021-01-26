2d6274c2b5bad691cb6bcc537ea1774218eae156
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-26 07:37:09 +0600

    basic_object_declaration_literal_vs_constrauctor

---
+++ 35.basic_object_declaration_literal_vs_constrauctor.js
@@ -0,0 +1,36 @@
+// obj declaration
+
+// var obj = {x:10, y:20, z:30}
+// console.log(obj)
+// console.log(typeof obj)
+
+// var obj = {}
+//   obj.x= 10
+// console.log(obj)
+
+//object literal
+// var obj = {
+//   x: 10,
+//   y: 20
+// }
+// console.log(obj)
+
+//object insert
+var point = {
+  x: 10,
+  y: 20
+}
+// point.y = 40
+point.z = 30
+console.log(point)
+
+//object constructor
+
+var obj = Object()
+obj.a = 50
+obj.c = 70
+console.log(obj)
+
+var obj2 = new Object()
+obj2.b = 60
+console.log(obj2)
