cb7c6fa538d33fc0306c8a68d455a76f7249625f
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-27 18:54:30 +0600

    Function_Declaration

---
+++ 42.Function_Declaration.js
@@ -0,0 +1,84 @@
+// define of function
+function functionName() {
+
+}
+
+// define of function
+function function_Name() {
+  console.log('I Am Mr. Emran Hasan')
+}
+function_Name()
+
+//
+
+function add() {
+  var a = 10
+  var b = 20
+  console.log(a+b)
+}
+add()
+//
+
+function sub() {
+  var a = 50
+  var b = 40
+  console.log(a-b)
+
+}
+sub()
+
+//
+function vdvd(a, b){
+  console.log(a, b)
+}
+vdvd('v', 'm')
+vdvd(1, 2)
+
+//basic
+function sample(a, b){
+  var c = a + b
+  var d = a - b
+  return c
+}
+console.log(sample(5, 1))
+//
+function sample(a, b){
+  var c = a + b
+  var d = a - b
+  return c+d
+}
+console.log(sample(5, 1))
+//
+function sample(a, b){
+  var c = a + b
+  console.log(c)
+}
+sample(5, 1)
+//
+function sample(a, b){
+  var c = a + b
+  var d = a - b
+  console.log(c+d)
+}
+sample(5, 1)
+
+
+//
+
+function sample(a, b){
+  var c = a + b
+  var d = a - b
+  var result = c + d
+  return result
+}
+console.log(sample(5, 1))
+
+//
+
+function sample(a, b){
+  var c = a + b
+  var d = a - b
+  var result = c + d
+  console.log(result)
+}
+sample(5, 1)
