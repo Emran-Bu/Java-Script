e30c2f12ca78aa1195d8ab72a03b0e9a88ef7dd6
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-27 18:57:00 +0600

    function_Invoking

---
+++ 43.function_Invoking.js
@@ -0,0 +1,32 @@
+// define of function
+function function_Name() {
+  console.log('I Am Mr. Emran Hasan')
+}
+function_Name()
+//
+
+function add() {
+  var a = 10
+  var b = 20
+  console.log(a+b)
+}
+add()
+//
+
+function sub() {
+  var a = 50
+  var b = 40
+  console.log(a-b)
+
+}
+sub()
+
+function emran(){
+  console.log('I am Mr. Emran')
+}
+// emran()
+// emran()
+
+for (var i = 0; i < 10; i++){
+  emran()
+}
