7ed876a5a539e4736950042a4c65db5e52c65692
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-01-29 08:05:05 +0600

    Pure_Function

---
+++ 49.Pure_Function.js
@@ -0,0 +1,36 @@
+//What is pure function?
+
+// It returns the same result if given the same arguments
+// It dose not cause any observable side effects
+
+// pure function
+function sqr(n){
+  return n*n
+}
+
+console.log(sqr(2))
+console.log(sqr(2))
+console.log(sqr(2))
+
+// not a pure function
+
+var n = 10
+
+function change(){
+  n = 100
+}
+change()
+console.log(n)
+
+var point = {
+  x: 45,
+  y: 30
+}
+function printPoint(point){
+  point.x = 100
+  point.y = 200
+  console.log(point)
+}
+console.log(point)
+printPoint(point)
+console.log(point)
