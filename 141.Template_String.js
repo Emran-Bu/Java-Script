b93ad09d1c9c89a8c477dcdd1e746dd8db343073
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-05-03 13:01:06 +0600

    Template String

---
+++ 141.Template_String.js
@@ -0,0 +1,15 @@
+// var x = 'emran' + 'eshan'
+// console.log(x)
+
+// var x = `ggg
+// dfgfdg        dfgdfgd
+//       fggfhfgh`
+// console.log(x)
+
+// Normal sring
+var age = 16
+var name = 'Emran'
+console.log('My name is ' + name + ' i am ' + age + ' years old' )
+// Template sring
+console.log(`My name is ${name} and i am ${age} years old`)
+console.log(`My name is ${name} and i am ${age} years old. I am ${age < 18 ? `not `: ``}adult.`)
